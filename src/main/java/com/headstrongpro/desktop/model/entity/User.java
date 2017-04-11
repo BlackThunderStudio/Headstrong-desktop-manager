f92e21c310c54eb9a3fbf1ba5d871759c6a3a71d
andre~ <katona.andrei@gmail.com>
2017-04-11 08:30:15 +0200

    smol user entity fix

--- src/main/java/com/headstrongpro/desktop/model/entity/User.java
+++ src/main/java/com/headstrongpro/desktop/model/entity/User.java
@@ -4,12 +4,10 @@ package com.headstrongpro.desktop.model.entity;
  * User Entity
  */
 public class User extends Person {
-    private int uid;
     private String cpr, street, postal, city, country, accountNo, baseSalary;
 
-    public User(int id, String name, String email, String phone, String gender, int uid, String cpr, String street, String postal, String city, String country, String accountNo, String baseSalary) {
+    public User(int id, String name, String email, String phone, String gender, String cpr, String street, String postal, String city, String country, String accountNo, String baseSalary) {
         super(id, name, email, phone, gender);
-        this.uid = uid;
         this.cpr = cpr;
         this.street = street;
         this.postal = postal;
@@ -17,14 +15,6 @@ public class User extends Person {
         this.country = country;
         this.accountNo = accountNo;
         this.baseSalary = baseSalary;
-    }
-
-    public int getUid() {
-        return uid;
-    }
-
-    public void setUid(int uid) {
-        this.uid = uid;
     }
 
     public String getCpr() {
