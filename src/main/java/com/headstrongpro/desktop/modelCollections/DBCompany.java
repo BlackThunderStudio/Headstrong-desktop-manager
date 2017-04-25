cda0bda27d69af1d4d0d8c9b1a942db2cbc10292
andre~ <katona.andrei@gmail.com>
2017-04-25 10:25:46 +0200

    added idao, sync ex, empty db company and db course cat.

---
+++ src/main/java/com/headstrongpro/desktop/modelCollections/DBCompany.java
@@ -0,0 +1,11 @@
+package com.headstrongpro.desktop.modelCollections;
+
+import com.headstrongpro.desktop.connection.IDataAccessObject;
+import com.headstrongpro.desktop.model.entity.Company;
+
+/**
+ * Created by 1062085 on 25-Apr-17.
+ */
+public class DBCompany implements IDataAccessObject<Company> {
+    //to be made
+}
---
+++ src/main/java/com/headstrongpro/desktop/modelCollections/DBCourseCategory.java
@@ -0,0 +1,11 @@
+package com.headstrongpro.desktop.modelCollections;
+
+import com.headstrongpro.desktop.connection.IDataAccessObject;
+import com.headstrongpro.desktop.model.CourseCategory;
+
+/**
+ * Created by 1062085 on 25-Apr-17.
+ */
+public class DBCourseCategory implements IDataAccessObject<CourseCategory> {
+    //to be made
+}
