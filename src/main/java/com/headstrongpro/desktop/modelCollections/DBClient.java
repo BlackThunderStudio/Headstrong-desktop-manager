295ac5aa15efb045fddf515d7b42224c07f7254e
andre~ <katona.andrei@gmail.com>
2017-05-09 12:40:41 +0200

    almost fix'd dbclient

--- src/main/java/com/headstrongpro/desktop/modelCollections/DBClient.java
+++ src/main/java/com/headstrongpro/desktop/modelCollections/DBClient.java
@@ -4,9 +4,11 @@ import com.headstrongpro.desktop.core.connection.DBConnect;
 import com.headstrongpro.desktop.core.connection.IDataAccessObject;
 import com.headstrongpro.desktop.core.exception.ConnectionException;
 import com.headstrongpro.desktop.core.exception.ModelSyncException;
+import com.headstrongpro.desktop.model.entity.Client;
 import com.headstrongpro.desktop.model.entity.EntityFactory;
 import com.headstrongpro.desktop.model.entity.Person;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -43,29 +45,89 @@ class DBClient implements IDataAccessObject<Person> {
         } catch (ConnectionException | SQLException e) {
             throw new ModelSyncException("Could not retrieve the clients!", e);
         }
-        return null;
+        return clients;
     }
 
     @Override
     public Person getById(int id) throws ModelSyncException {
-        //TODO: to be implemented
-        return null;
+        Person client = null;
+        try{
+            connect = new DBConnect();
+            ResultSet rs = connect.getFromDataBase("SELECT * FROM clients WHERE id=" + id + ";");
+            rs.next();
+            client = new Client(rs.getInt("id"),
+                                rs.getString("name"),
+                                rs.getString("email"),
+                                String.valueOf(rs.getInt("phone_number")),
+                                String.valueOf(rs.getBoolean("gender")),
+                                rs.getString("login"),
+                                rs.getString("pass"),
+                                rs.getDate("date_registered"),
+                                rs.getInt("company_id"));
+        } catch (ConnectionException | SQLException e) {
+          throw new ModelSyncException("Could not retrieve the clients!", e);
+        }
+        return client;
     }
 
     @Override
     public Person create(Person object) throws ModelSyncException {
-        //TODO: to be implemented
-        return null;
+        Client newClient = (Client) object;
+        try {
+            connect = new DBConnect();
+            String query = "INSERT INTO clients(name, email, phone_number, gender, login, pass, date_registered, company_id) VALUES (?,?,?,?,?,?,?,?);";
+            PreparedStatement preparedStatement = connect.getConnection().prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, newClient.getName());
+            preparedStatement.setString(2, newClient.getEmail());
+            preparedStatement.setString(3, newClient.getPhone());
+            preparedStatement.setString(4, newClient.getGender());
+            preparedStatement.setString(5, newClient.getLogin());
+            preparedStatement.setString(6, newClient.getPassword());
+            preparedStatement.setDate(7, newClient.getRegistrationDate());
+            preparedStatement.setInt(8, newClient.getcompanyId());
+            preparedStatement.executeUpdate();
+            try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    newClient.setId(generatedKeys.getInt(1));
+                } else {
+                    throw new ModelSyncException("Creating new client failed! Could not retrieve the ID!");
+                }
+            }
+        } catch (ConnectionException | SQLException e) {
+            throw new ModelSyncException("Could not create a client!", e);
+        }
+        return newClient;
     }
 
     @Override
     public void update(Person object) throws ModelSyncException {
-        //TODO: to be implemented
+        Client client = (Client) object;
+        try{
+            connect = new DBConnect();
+            String query = "UPDATE clients SET name=?, email=?, phone_number=?, gender=?, login=?, pass=?, date_registered=?, company_id=?;";
+            PreparedStatement preparedStatement = connect.getConnection().prepareStatement(query);
+            preparedStatement.setString(1, client.getName());
+            preparedStatement.setString(2, client.getEmail());
+            preparedStatement.setString(3, client.getPhone());
+            preparedStatement.setString(4, client.getGender());
+            preparedStatement.setString(5, client.getLogin());
+            preparedStatement.setString(6, client.getPassword());
+            preparedStatement.setDate(7, client.getRegistrationDate());
+            preparedStatement.setInt(8, client.getcompanyId());
+            connect.uploadSafe(preparedStatement);
+        } catch (ConnectionException | SQLException e) {
+            throw new ModelSyncException("Could not update a client!", e);
+        }
     }
 
     @Override
     public void delete(Person object) throws ModelSyncException {
-        //TODO: to be implemented
+        try{
+            connect = new DBConnect();
+            connect.upload("DELETE FROM clients WHERE id=" + object.getId() + ";");
+        } catch (ConnectionException e) {
+            throw new ModelSyncException("Could not delete a client!", e);
+        }
     }
 
     public List<Person> getByCompanyId(int id) {
