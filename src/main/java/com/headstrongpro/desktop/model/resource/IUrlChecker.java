77d9c60622633feafa3f280d04d7f18a61979495
Rajmund Staniek <rajmund.st@gmail.com>
2017-05-09 16:43:44 +0200

    added annotations

--- src/main/java/com/headstrongpro/desktop/model/resource/IUrlChecker.java
+++ src/main/java/com/headstrongpro/desktop/model/resource/IUrlChecker.java
@@ -3,6 +3,7 @@ package com.headstrongpro.desktop.model.resource;
 /**
  * Created by Rajmund Staniek on 09-May-17.
  */
+@FunctionalInterface
 public interface IUrlChecker {
     boolean check(String url);
 }
--- src/main/java/com/headstrongpro/desktop/model/resource/IUrlTransformer.java
+++ src/main/java/com/headstrongpro/desktop/model/resource/IUrlTransformer.java
@@ -3,6 +3,7 @@ package com.headstrongpro.desktop.model.resource;
 /**
  * Created by Rajmund Staniek on 09-May-17.
  */
+@FunctionalInterface
 public interface IUrlTransformer {
     String transform(String url);
 }
