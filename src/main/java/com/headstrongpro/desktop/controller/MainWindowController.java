868285c0572fe24a2f3f34a9a771a05dd902502e
Rajmund <rajmund.st@gmail.com>
2017-05-17 14:28:22 +0200

    our first easter egg

--- src/main/java/com/headstrongpro/desktop/controller/MainWindowController.java
+++ src/main/java/com/headstrongpro/desktop/controller/MainWindowController.java
@@ -1,7 +1,22 @@
 package com.headstrongpro.desktop.controller;
 
+import javafx.fxml.Initializable;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by rajmu on 17.04.06.
  */
-public class MainWindowController {
+public class MainWindowController implements Initializable {
+    public WebView webViw;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        WebEngine engine = webViw.getEngine();
+        engine.load("http://headstrongpro.com/alt/maps/");
+
+    }
 }
