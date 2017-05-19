3a7637e960d32979094b7a2685a92af9ec05303f
Ondřej Soukup <enes.vint@gmail.com>
2017-05-19 18:46:59 +0200

    Maven fixed up with TilesFX, dashboard empty for now cuz documentation is cancar

---
+++ src/main/java/com/headstrongpro/desktop/controller/DashboardPaneController.java
@@ -0,0 +1,17 @@
+package com.headstrongpro.desktop.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Ondřej Soukup on 19.05.2017.
+ */
+public class DashboardPaneController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
