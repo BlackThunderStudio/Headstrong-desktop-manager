f4ec35ba1b065845d5b5fca97ccc39fb55b2e46f
andre~ <katona.andrei@gmail.com>
2017-05-26 21:30:46 +0200

    too many files I omited this homoclient

--- src/main/java/com/headstrongpro/desktop/core/controller/ClientsController.java
+++ src/main/java/com/headstrongpro/desktop/core/controller/ClientsController.java
@@ -1,6 +1,7 @@
 package com.headstrongpro.desktop.core.controller;
 
 import com.headstrongpro.desktop.DbLayer.DBClient;
+import com.headstrongpro.desktop.core.exception.DatabaseOutOfSyncException;
 import com.headstrongpro.desktop.core.exception.ModelSyncException;
 import com.headstrongpro.desktop.model.entity.Client;
 import com.headstrongpro.desktop.model.entity.Person;
@@ -35,6 +36,15 @@ public class ClientsController implements Refreshable{
                 e.getEmail().toLowerCase().contains(keyword.toLowerCase()) ||
                 e.getPhone().toLowerCase().contains(keyword.toLowerCase()) ||
                 e.getGender().toLowerCase().contains(keyword.toLowerCase())).collect(Collectors.toList()));
+    }
+
+    public void updateClient(int id, String name, String email, String phone, String gender) throws ModelSyncException, DatabaseOutOfSyncException{
+        Client client = (Client) clientsDAO.getById(id);
+        client.setName(name);
+        client.setEmail(email);
+        client.setPhone(phone);
+        client.setGender(gender);
+        clientsDAO.update(client);
     }
 
     @Override
