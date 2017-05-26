8dc1b8299dda673b2a0e139d2bcabc5a9c7da0cd
andre~ <katona.andrei@gmail.com>
2017-05-26 16:58:49 +0200

    clients tests

---
+++ src/test/java/com/headstrongpro/desktop/core/controller/ClientsControllerTest.java
@@ -0,0 +1,32 @@
+package com.headstrongpro.desktop.core.controller;
+
+import com.headstrongpro.desktop.DbLayer.DBClient;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * clients controler test
+ */
+public class ClientsControllerTest {
+    ClientsController clientsController;
+    DBClient clientDAO;
+
+    @Before
+    public void setUp() throws Exception {
+        clientsController = new ClientsController();
+        clientDAO = new DBClient();
+    }
+
+    @Test
+    public void getClients() throws Exception {
+        assertNotNull(clientsController.getClients());
+    }
+
+    @Test
+    public void search() throws Exception {
+        assertEquals("johnnn99@neasenergy.com", clientsController.search("john").get(0).getEmail());
+    }
+
+}
