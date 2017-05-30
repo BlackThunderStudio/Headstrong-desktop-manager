cd8f99510ebdb82869d9aa612568451a661b3391
Rajmund Staniek <rajmund.st@gmail.com>
2017-05-30 18:04:44 +0200

    updated test for tiles

--- src/main/java/com/headstrongpro/desktop/core/fxControls/Test.java
+++ src/main/java/com/headstrongpro/desktop/core/fxControls/Test.java
@@ -11,14 +11,33 @@ import java.util.ResourceBundle;
  * Created by Rajmund Staniek on 25-May-17.
  */
 public class Test implements Initializable {
-    public DashboardTile tile1;
+
+
+    public DashboardTile t1;
+    public DashboardTile t2;
+    public DashboardTile t3;
+    public DashboardTile t4;
+    public DashboardTile t5;
+    public DashboardTile t6;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        tile1.title.setText("Sexy title");
-        tile1.value.setText("20");
-        tile1.subtitle.setText("fucks given today");
-        tile1.setTextFill(Color.web("#eeeeee"));
-        tile1.pane.setStyle("-fx-background-color: #323232");
+        t1.setTextFill(Color.web("#eeeeee"));
+        t2.setTextFill(Color.web("#eeeeee"));
+        t3.setTextFill(Color.web("#eeeeee"));
+        t4.setTextFill(Color.web("#eeeeee"));
+        t5.setTextFill(Color.web("#eeeeee"));
+        t6.setTextFill(Color.web("#eeeeee"));
+
+        t1.setBackgroundColor("#323232");
+        t2.setBackgroundColor("#323232");
+        t3.setBackgroundColor("#323232");
+        t4.setBackgroundColor("#323232");
+        t5.setBackgroundColor("#323232");
+        t6.setBackgroundColor("#323232");
+
+        t1.setTitle("Companies");
+        t1.setSubtitle("Clients this month");
+        t1.setValue("+5");
     }
 }
