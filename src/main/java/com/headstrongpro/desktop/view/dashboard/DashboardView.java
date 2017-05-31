3c24713be10654eb85978c7642460db0223c77a1
Jakub Slíva <sliva.jakub@gmail.com>
2017-05-31 19:27:25 +0200

    got to load the dashboard, looks ugly but works

--- src/main/java/com/headstrongpro/desktop/view/dashboard/DashboardView.java
+++ src/main/java/com/headstrongpro/desktop/view/dashboard/DashboardView.java
@@ -1,17 +1,17 @@
 package com.headstrongpro.desktop.view.dashboard;
 
 import com.headstrongpro.desktop.core.fxControls.DashboardTile;
-import eu.hansolo.tilesfx.Tile;
+import com.headstrongpro.desktop.view.ContentView;
+import com.headstrongpro.desktop.view.MainWindowView;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
  * Created by Ondřej Soukup on 19.05.2017.
  */
-public class DashboardView implements Initializable {
+public class DashboardView extends ContentView implements Initializable {
 
     @FXML
     public DashboardTile dashCompaniesTile;
@@ -27,6 +27,10 @@ public class DashboardView implements Initializable {
     public DashboardTile dashResourcesTile;
     @FXML
     public DashboardTile dashSettingsTile;
+    @FXML
+    public DashboardTile dashEmployeesTile;
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
