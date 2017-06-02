aa3d02b48767448948299773a242aece29bc18fb
Rajmund Staniek <rajmund.st@gmail.com>
2017-06-02 16:13:18 +0200

    added more functionality to subs

--- src/main/java/com/headstrongpro/desktop/controller/SubscriptionsController.java
+++ src/main/java/com/headstrongpro/desktop/controller/SubscriptionsController.java
@@ -2,18 +2,22 @@ package com.headstrongpro.desktop.controller;
 
 import com.headstrongpro.desktop.DbLayer.DBSubscriptions;
 import com.headstrongpro.desktop.core.exception.ConnectionException;
+import com.headstrongpro.desktop.core.exception.DatabaseOutOfSyncException;
 import com.headstrongpro.desktop.core.exception.ModelSyncException;
 import com.headstrongpro.desktop.model.PaymentRate;
 import com.headstrongpro.desktop.model.Subscription;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.bouncycastle.math.raw.Mod;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Subscriptions Controller
  */
-public class SubscriptionsController implements Refreshable {
+public class SubscriptionsController implements Refreshable, IContentController<Subscription> {
 
     private List<Subscription> subscriptions;
     private DBSubscriptions subscriptionDAO;
@@ -23,9 +27,9 @@ public class SubscriptionsController implements Refreshable {
         subscriptionDAO = new DBSubscriptions();
     }
 
-    public List<Subscription> getSubscriptions() throws ModelSyncException{
+    public ObservableList<Subscription> getAll() throws ModelSyncException{
         refresh();
-        return subscriptions;
+        return FXCollections.observableArrayList(subscriptions);
     }
 
     @Override
@@ -36,5 +40,38 @@ public class SubscriptionsController implements Refreshable {
 
     public List<PaymentRate> getRates() throws ConnectionException {
         return subscriptionDAO.getRates();
+    }
+
+
+    @Override
+    public ObservableList<Subscription> searchByPhrase(String input) {
+        if(input == null) throw new IllegalArgumentException("Input cannot be null!");
+        return FXCollections.observableArrayList(subscriptions
+                .stream()
+                .filter(e -> e.getCompany().getName().toLowerCase().contains(input) ||
+                        e.getRate().getName().toLowerCase().contains(input) ||
+                        String.valueOf(e.getNoOfUsers()).contains(input))
+                .collect(Collectors.toList()));
+    }
+
+    @Override
+    public void delete(Subscription subscription) throws DatabaseOutOfSyncException, ModelSyncException {
+        subscriptionDAO.delete(subscription);
+        refresh();
+    }
+
+    @Override
+    public Subscription createNew(Subscription subscription) throws ModelSyncException {
+        return null;
+    }
+
+    @Override
+    public void edit(Subscription subscription) throws DatabaseOutOfSyncException, ModelSyncException {
+
+    }
+
+    @Override
+    public Subscription getByID(int id) throws ModelSyncException {
+        return null;
     }
 }
